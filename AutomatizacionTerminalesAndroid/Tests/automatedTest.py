70ba5265f8cc091a1b3de2c7206603e394735bfe
Lucas Hartman <lucas.hartman@tsoftglobal.com>
2021-07-27 09:45:14 -0300

    subida para merge

--- AutomatizacionTerminalesAndroid/Tests/automatedTest.py
+++ AutomatizacionTerminalesAndroid/Tests/automatedTest.py
@@ -1,4 +1,6 @@
 import pytest
+import unittest
+from selenium import webdriver
 from AutomatizacionTerminalesAndroid.Classes.base import Base
 from AutomatizacionTerminalesAndroid.Classes.installmentsPage import InstallmentsPage
 from AutomatizacionTerminalesAndroid.Classes.mainPage import MainPage
@@ -10,7 +12,7 @@ class TestCases:
 
 
     def setup(self):
-        self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', Base.return_base(Base()))
+        self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', Base.return_base())
         driver = self.driver
         return driver
 
@@ -20,8 +22,10 @@ class TestCases:
         self.main_page = MainPage(driver)
         self.tickets_page = TicketPage(driver)
         self.main_page.enter_amount("1234")
-        assert self.main_page.payment_option("card") is True
-
+        self.main_page.payment_option("card")
+        self.tickets_page.print_client_ticket()
+        self.tickets_page.btn_continue()
+        self.tickets_page.btn_finish()
 
 if __name__ == '__main__':
     pytest.main(["-v", "--alluredir=/tmp/my_allure_results", "C:/Users/lucas.hartman/Documents/Lucas/Tsoft/prisma/AutomatizacionTerminales/AutomatizacionTerminalesAndroid/Tests/automatedTest.py"])
